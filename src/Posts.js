5cfe5e94245d8ef5954405eb142dc3351616dc4d
l.petukhova_8 <148487155+anapa2013@users.noreply.github.com>
2025-10-04 16:56:47 +0300

    change posts html

--- src/Posts.js
+++ src/Posts.js
@@ -1,24 +1,63 @@
 import "./Posts.css"
+import like from "./img/like.png"
+import comment from "./img/comment.png"
+import repost from "./img/repost.png"
 
-export function Post(){
+let posts = [
+    {
+        id:0,
+        imageLink: "https://github.com/anapa2013/social_net/blob/main/public/media/photo%201.jpeg?raw=true",
+        text: "Солнечные лучи пробиваются сквозь густую листву, освещая поляну. В воздухе пахнет хвоей и свежестью. Тишину нарушает лишь шелест листьев и пение птиц.",
+        likes: 6,
+        comments: [
+            "wow!", 
+            "text from ai",
+        ]
+    },
+    {
+        id:1,
+        imageLink: "https://github.com/anapa2013/social_net/blob/main/public/media/photo%201.jpeg?raw=true",
+        text: "Солнечные лучи пробиваются сквозь густую листву, освещая поляну. В воздухе пахнет хвоей и свежестью. Тишину нарушает лишь шелест листьев и пение птиц.",
+        likes: 6,
+        comments: [
+            "wow!", 
+            "text from ai",
+        ]
+    },
+    {
+        id:2,
+        imageLink: "https://github.com/anapa2013/social_net/blob/main/public/media/photo%201.jpeg?raw=true",
+        text: "Солнечные лучи пробиваются сквозь густую листву, освещая поляну. В воздухе пахнет хвоей и свежестью. Тишину нарушает лишь шелест листьев и пение птиц.",
+        likes: 6,
+        comments: [
+            "wow!", 
+            "text from ai",
+        ]
+    }
+]
+
+export function Post({id, imageLink, text, likes, comments}){
     return (
         <div className="post">
-            <img className="post-img"/>
-            <p className="post-text"></p>
+            <img className="post-img" src={imageLink}/>
+            <p className="post-text">{text}</p>
             <div className="post-nav">
-                <ul>
-                    <li className="nav-item"><img /></li>
-                    <li className="nav-item"><img /></li>
-                    <li className="nav-item"><img /></li>
-                </ul>
+               <span className="likes">{likes} <img src={like}/> </span>
+               <span className="comments"><img src={comment}/></span>
+               <span className="repost"><img src={repost}/></span>
             </div>
         </div>
     )
 }
 export function Posts(){
+
+    let postsDisplay = posts.map((post)=>(
+        <Post id={post.id} imageLink={post.imageLink} text={post.text} likes={post.likes} comments={post.comments}></Post>
+    ))
+
     return (
         <div className="posts">
-            <Post></Post>
+            {postsDisplay}
         </div>
     )
 }
